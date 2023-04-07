f9c6288b7c160118c27d1341f8d73b3436d601b3
Ibtehaz Rafid <IbtehazR@nycstudents.net>
2023-04-06 22:05:14 -0400

    uno

--- src/Point.java
+++ src/Point.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Point {
+public class Point {
+    private int x;
+    private int y;
+    private String label;
+
+    public Point(String label, int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.label = label;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || obj instanceof String) {
+            return false;
+        }
+        return toString().substring(14).equals(obj.toString().substring(14));
+    }
+
+    public String toString() {
+        return ("Point " + label + " is at " + "x = " + x + ", y = " + y);
+    }
 }
--- src/ThreeDPoint.java
+++ src/ThreeDPoint.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class ThreeDPoint {
+public class ThreeDPoint extends Point {
+    private int z;
+
+    public ThreeDPoint(String label, int x, int y, int z) {
+        super(label, x, y);
+        this.z = z;
+    }
+
+    public String toString() {
+        return super.toString() + ", z = " + z;
+    }
 }
